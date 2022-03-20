0e56bedf50cabafeaa00295eb79e2dfc42edce57
EyalAkibli <63187468+EyalAkibli@users.noreply.github.com>
2022-03-20 18:16:26 +0200

    home_activity: Add activity to facilitate toolbar
    Signed-off-by: EyalAkibli <63187468+EyalAkibli@users.noreply.github.com>

---
+++ app/src/main/java/com/example/wellibe/Home.java
@@ -0,0 +1,14 @@
+package com.example.wellibe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends WelliBeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
