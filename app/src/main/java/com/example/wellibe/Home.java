dd3f0d3f4bfc499bb8d44b21a0aeff06eacd08b8
EyalAkibli <63187468+EyalAkibli@users.noreply.github.com>
2022-03-20 23:37:42 +0200

    general: Add drawer layout support
    Added support for the drawer in the base activity (aka WelliBeActivity).
    Added function to manage drawer actions.
    Improved resorces: themes, string etc.
    Improved toolbar options support.
    
    Signed-off-by: EyalAkibli <63187468+EyalAkibli@users.noreply.github.com>

--- app/src/main/java/com/example/wellibe/Home.java
+++ app/src/main/java/com/example/wellibe/Home.java
@@ -1,14 +1,32 @@
 package com.example.wellibe;
 
+import static com.example.wellibe.WelliBeActivity.ToolBarMode.NO_BUTTONS;
+import static com.example.wellibe.WelliBeActivity.ToolBarMode.ONLY_BACK;
+import static com.example.wellibe.WelliBeActivity.ToolBarMode.ONLY_HAMBURGER;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class Home extends WelliBeActivity {
 
+    NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        toolBarMode = ONLY_HAMBURGER;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        initToolbar();
+
+        setupDrawerContentActions(findViewById(R.id.navigation_view));
+
     }
 }
--- app/src/main/java/com/example/wellibe/WelliBeActivity.java
+++ app/src/main/java/com/example/wellibe/WelliBeActivity.java
@@ -1,10 +1,19 @@
 package com.example.wellibe;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class WelliBeActivity extends AppCompatActivity {
     /**
@@ -20,12 +29,74 @@ public class WelliBeActivity extends AppCompatActivity {
         ONLY_HAMBURGER
     }
 
+    ToolBarMode toolBarMode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        //getSupportActionBar().setDisplayShowTitleEnabled(false);
 
     }
 
+    public void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.tool_bar_back_menu);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        setSupportActionBar(toolbar);
+        switch (this.toolBarMode) {
+            case NO_BUTTONS:
+                getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+                drawerLayout.setDrawerLockMode(DrawerLayout. LOCK_MODE_LOCKED_CLOSED);
+                break;
+            case ONLY_BACK:
+                drawerLayout.setDrawerLockMode(DrawerLayout. LOCK_MODE_LOCKED_CLOSED);
+                break;
+            case ONLY_HAMBURGER:
+                getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+                DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                        R.string.open_drawer,R.string.close_drawer);
+
+                drawer.addDrawerListener(drawerToggle);
+                drawerToggle.syncState();
+                break;
+            default: //ONLY_BACK
+                break;
+        }
+    }
+    protected void setupDrawerContentActions(@NonNull NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.home:
+
+                    break;
+                case R.id.add_new_visit:
+
+                    break;
+                case R.id.view_my_visits:
+
+                    break;
+                case R.id.logout:
+                    //LogoutProcedure();
+                    Toast.makeText(getApplicationContext(), "Logging out..", Toast.LENGTH_SHORT).show();
+                    Intent backToSignIn = new Intent(getApplicationContext(), SignIn.class);
+                    finish();
+                    startActivity(backToSignIn);
+                    break;
+            }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        final DrawerLayout drawerLeft = findViewById(R.id.drawer_layout);
+        if (drawerLeft != null && drawerLeft.isDrawerOpen(GravityCompat.START)) {
+            drawerLeft.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
